523b5226525c5d099d7afb49392b7f284affc14c
Sean Devlin <francoisdevlin@gmail.com>
2015-02-09 12:51:11 -0500

    Added some thoughts in comment, moved fn

--- main.go
+++ main.go
@@ -1,74 +1,60 @@
-//Things to generate
-//Go type parser
-//Type JSON Marshall/UnMarshall (DONE!  Thanks Go!)
-//CLI interface (Action Framework)
-//Web Component
-//Ext Model
-//Ext Store
-//Ext Table
-//Ext Default Form
-//JSON Marshall/UnMarshall (leverage existing, quick hack)
-//Protobuff Marshall/UnMarshall
-//Avro Marshall/UnMarshall
-//EDN Marshall/UnMarshall
-//map[string]interaface{} reader
-//Curses Interface
-//SQLite table creation
-//SQLite record maniputalor
-// * Create
-// * READ 1
-// * READ WHERE (Excel autofilter)
-// * Update 1
-// * Delete 1
-//MySQL table creation
-//MySQL record maniputalor
-//Standard REST endpoints
-//"Nominal Dropdowns" from SQL backend
+/*
+Things to generate
+
+* Tools for generating type info
+** Type info factories
+*** Go type parser
+*** Type JSON Marshall/UnMarshall (DONE!  Thanks Go!)
+** Type info sanity
+*** Combine with unit test, verify annotations are correct
+*** Only one autoinc
+*** Nominal with unique field
+** Type conventions
+*** Primary Key convention (Id)
+*** Nominal Convention
+
+* Consumers of type info
+** CLI interface (Think Action Framework)
+** Curses Interface
+** GUI
+*** Web Component
+*** Ext Model
+*** Ext Store
+*** Ext Table
+*** Ext Default Form
+*** Ext Nominal Dropdown
+*** Ext Nominal Autocomplete
+** Marshalling
+*** JSON Marshall/UnMarshall (leverage existing, quick hack)
+*** XML Marshall/UnMarshall (leverage existing, quick hack)
+*** Protobuff Marshall/UnMarshall
+*** Avro Marshall/UnMarshall
+*** EDN Marshall/UnMarshall
+** SQL
+*** Table creation
+*** Insert
+*** READ 1
+*** READ WHERE (Excel autofilter)
+*** Update 1
+*** Delete 1
+** Standard REST endpoints
+*** Create
+*** Read
+*** Update
+*** Delete
+*** List Where...
+*** Live Docs
+*** Nominal Read endpoint
+*/
 package main
 
 import (
 	//"database/sql"
 	"fmt"
-	"reflect"
+	//"reflect"
 	//"strconv"
 	//"strings"
 )
-
-func getInfo(fields []FieldDescription) (output []Info) {
-	for i, field := range fields {
-		temp := Info{
-			FieldInfo:     field.GetFieldInfo(),
-			SqlInfo:       field.GetFieldSqlInfo(),
-			UiInfo:        field.GetFieldUiInfo(),
-			ValidatorInfo: field.GetFieldValidatorInfo(),
-		}
-		if temp.FieldOrder == 0 {
-			temp.FieldOrder = int64(i)
-		}
-		output = append(output, temp)
-	}
-
-	return output
-}
-
-func GetInfo(record interface{}) (output []Info) {
-	typ := reflect.TypeOf(record)
-	// if a pointer to a struct is passed, get the type of the dereferenced object
-	if typ.Kind() == reflect.Ptr {
-		typ = typ.Elem()
-	}
-
-	// loop through the struct's fields and set the map
-	fields := make([]FieldDescription, 0)
-	for i := 0; i < typ.NumField(); i++ {
-		field := typ.Field(i)
-		fields = append(fields, reflectValue(field))
-	}
-
-	output = getInfo(fields)
-
-	return output
-}
 
 type SqlHydration interface {
 	GetRecords(ids []int)
@@ -79,7 +65,5 @@ type SqlHydration interface {
 }
 
 func main() {
-	//GetRecords([]int{1, 2, 3}, &[]int{})
-	//CreateSQLiteTable(&Foo{})
 	fmt.Println("Hello")
 }
--- reflect_value.go
+++ reflect_value.go
@@ -43,3 +43,40 @@ func (field reflectValue) GetFieldUiInfo() (output UiInfo) {
 func (field reflectValue) GetFieldValidatorInfo() (output ValidatorInfo) {
 	return output
 }
+
+//These don't really go here...
+func getInfo(fields []FieldDescription) (output []Info) {
+	for i, field := range fields {
+		temp := Info{
+			FieldInfo:     field.GetFieldInfo(),
+			SqlInfo:       field.GetFieldSqlInfo(),
+			UiInfo:        field.GetFieldUiInfo(),
+			ValidatorInfo: field.GetFieldValidatorInfo(),
+		}
+		if temp.FieldOrder == 0 {
+			temp.FieldOrder = int64(i)
+		}
+		output = append(output, temp)
+	}
+
+	return output
+}
+
+func GetInfo(record interface{}) (output []Info) {
+	typ := reflect.TypeOf(record)
+	// if a pointer to a struct is passed, get the type of the dereferenced object
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	// loop through the struct's fields and set the map
+	fields := make([]FieldDescription, 0)
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		fields = append(fields, reflectValue(field))
+	}
+
+	output = getInfo(fields)
+
+	return output
+}
