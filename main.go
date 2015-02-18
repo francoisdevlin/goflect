126c5dfbf63c84f98096d885840678fbb99d03a7
Sean Devlin <francoisdevlin@gmail.com>
2015-02-18 12:54:54 -0500

    Added README

--- main.go
+++ main.go
@@ -1,61 +1,58 @@
-/*
-Things to generate
-
-* Tools for generating type info
-** Type info factories
-*** Go type parser
-*** Type JSON Marshall/UnMarshall (DONE!  Thanks Go!)
-** Type info sanity
-*** Combine with unit test, verify annotations are correct
-*** Only one autoinc
-*** Nominal with unique field
-** Type conventions
-*** Primary Key convention (Id)
-*** Nominal Convention
-
-* Consumers of type info
-** CLI interface (Think Action Framework)
-** Curses Interface
-** GUI
-*** Web Component
-*** Ext Model
-*** Ext Store
-*** Ext Table
-*** Ext Default Form
-*** Ext Nominal Dropdown
-*** Ext Nominal Autocomplete
-** Marshalling
-*** JSON Marshall/UnMarshall (leverage existing, quick hack)
-*** XML Marshall/UnMarshall (leverage existing, quick hack)
-*** Protobuff Marshall/UnMarshall
-*** Avro Marshall/UnMarshall
-*** EDN Marshall/UnMarshall
-** SQL
-*** Table creation
-*** Insert
-*** READ 1
-*** READ WHERE (Excel autofilter)
-*** Update 1
-*** Delete 1
-** Standard REST endpoints
-*** Create
-*** Read
-*** Update
-*** Delete
-*** List Where...
-*** Live Docs
-*** Nominal Read endpoint
-*/
 package main
 
 import (
 	//"database/sql"
+	//"strings"
+	//"html/template"
+	//"net/http"
+	//"flag"
 	"fmt"
+	"os"
 	//"reflect"
 	//"strconv"
-	//"strings"
 )
 
 func main() {
-	fmt.Println("Hello")
+	type Bar struct {
+		I       int `desc:"This is the Id"`
+		I64     int64
+		I32     int32
+		I16     int16
+		I8      int8
+		U       uint `desc:"This is the Ud" default:"1234"`
+		U64     uint64
+		U32     uint32
+		U16     uint16
+		U8      uint8
+		F64     float64
+		F32     float32
+		S       string
+		Awesome bool `desc:"This controls the awesomeness"`
+	}
+
+	//record := Info{}
+	record := Bar{}
+	FlagSetup(&record, os.Args)
+	//fields := GetInfo(&record)
+
+	//flags := flag.FlagSet{}
+
+	//for _, field := range fields {
+	////fmt.Println(field)
+	//flag.String(field.Name, "", field.Description)
+	//flags = flags
+	//}
+
+	fmt.Println(record)
+
+	//handler := func(w http.ResponseWriter, r *http.Request) {
+	////t, _ := template.ParseFiles("header.html")
+	////t.Execute(w, "My Title")
+	//t, _ := template.ParseFiles("test.html")
+	//t.Execute(w, records)
+	////fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	////fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	//}
+	//http.HandleFunc("/", handler)
+	//http.ListenAndServe(":8080", nil)
 }
