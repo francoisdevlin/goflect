453adfecad55b9f352a8c1af46eb1e4cf075b452
Sean Devlin <francoisdevlin@gmail.com>
2015-02-20 09:40:53 -0500

    Added inverter, or, and matchers

--- matcher/utility.go
+++ matcher/utility.go
@@ -3,12 +3,70 @@ package goflect
 type AnyMatch int
 type NoneMatch int
 
+type InvertMatch struct {
+	M Matcher
+}
+
+type AndMatch struct {
+	Matchers []Matcher
+}
+
+type OrMatch struct {
+	Matchers []Matcher
+}
+
 func (a AnyMatch) Match(record interface{}) (bool, error) {
 	return true, nil
 }
 
 func (a NoneMatch) Match(record interface{}) (bool, error) {
 	return false, nil
+}
+
+func (a InvertMatch) Match(record interface{}) (bool, error) {
+	result, err := a.M.Match(record)
+	if err != nil {
+		return false, err
+	}
+	return !result, nil
+}
+
+func (a AndMatch) Match(record interface{}) (bool, error) {
+	accum := true
+	for _, m := range a.Matchers {
+		result, err := m.Match(record)
+		if err != nil {
+			return false, err
+		}
+		accum = accum && result
+		if !accum {
+			break
+		}
+	}
+	return accum, nil
+}
+
+func (a OrMatch) Match(record interface{}) (bool, error) {
+	accum := false
+	for _, m := range a.Matchers {
+		result, err := m.Match(record)
+		if err != nil {
+			return false, err
+		}
+		accum = accum || result
+		if accum {
+			break
+		}
+	}
+	return accum, nil
+}
+
+func Any() Matcher {
+	return AnyMatch(1)
+}
+
+func None() Matcher {
+	return NoneMatch(1)
 }
 
 func Eq(record interface{}) Matcher {
@@ -49,4 +107,16 @@ func Match(record string) Matcher {
 
 func NotMatch(record string) Matcher {
 	return FieldMatcher{Op: NOT_MATCH, Value: record}
+}
+
+func Not(matcher Matcher) Matcher {
+	return InvertMatch{M: matcher}
+}
+
+func And(matchers ...Matcher) Matcher {
+	return AndMatch{Matchers: matchers}
+}
+
+func Or(matchers ...Matcher) Matcher {
+	return OrMatch{Matchers: matchers}
 }
--- matcher/utility_test.go
+++ matcher/utility_test.go
@@ -7,13 +7,44 @@ import (
 func TestAnyMatch(t *testing.T) {
 	withMatcher := withMatcherFactory(t)
 
-	matchTrue, _, _ := withMatcher(AnyMatch(1))
+	matchTrue, _, _ := withMatcher(Any())
 	matchTrue("1 is Any", 1)
 }
 
 func TestNoneMatch(t *testing.T) {
 	withMatcher := withMatcherFactory(t)
 
-	_, matchFalse, _ := withMatcher(NoneMatch(1))
+	_, matchFalse, _ := withMatcher(None())
+	matchFalse("1 is not None", 1)
+}
+
+func TestInvertMatch(t *testing.T) {
+	withMatcher := withMatcherFactory(t)
+
+	matchTrue, _, _ := withMatcher(Not(None()))
+	matchTrue("1 is Any", 1)
+	_, matchFalse, _ := withMatcher(Not(Any()))
 	matchFalse("1 is Any", 1)
+}
+
+func TestAndMatch(t *testing.T) {
+	withMatcher := withMatcherFactory(t)
+
+	matchTrue, matchFalse, matchError := withMatcher(And(Gt(int(1)), Lt(int(10))))
+	matchFalse("1 is too small", int(1))
+	matchTrue("2 is okay", int(2))
+	matchTrue("9 is okay", int(9))
+	matchFalse("10 is too big", int(10))
+	matchError("Bacon is right out", "Bacon")
+}
+
+func TestOrMatch(t *testing.T) {
+	withMatcher := withMatcherFactory(t)
+
+	matchTrue, matchFalse, matchError := withMatcher(Or(Lte(int(1)), Gte(int(10))))
+	matchTrue("1 is okay", int(1))
+	matchFalse("2 is bad", int(2))
+	matchFalse("9 is bad", int(9))
+	matchTrue("10 is okay", int(10))
+	matchError("Bacon is right out", "Bacon")
 }
