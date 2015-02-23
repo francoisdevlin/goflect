c2bbcdcb84657b9c80af97e85c8d03b61441bc25
Sean Devlin <francoisdevlin@gmail.com>
2015-02-23 14:48:43 -0500

    Added a basic parser for marshalling matchers, WIP

---
+++ matcher/parse.go
@@ -0,0 +1,172 @@
+package goflect
+
+import (
+	"fmt"
+	//"sort"
+	//"strconv"
+	//"strings"
+	"regexp"
+)
+
+type ParseErrors int
+
+const (
+	VALID ParseErrors = iota
+	INVALID_OPERATION
+	UNFINISHED_MESSAGE
+	TOKENIZE_ERROR
+	UNKNOWN_FIELD
+)
+
+type MatchParseError struct {
+	Code    ParseErrors
+	Message string
+}
+
+func (s MatchParseError) Error() string {
+	return s.Message
+}
+
+type ParseStruct struct {
+	Fields map[string]int
+}
+
+func (service ParseStruct) Parse(input string) (Matcher, error) {
+	tokens, err := Tokenize(input)
+	if err != nil {
+		fmt.Println(tokens)
+		return nil, err
+	}
+	//output := StructMatcher{}
+	output := And()
+	Lookup := map[string]FieldOps{
+		"=":         EQ,
+		"!=":        NEQ,
+		"<":         LT,
+		"<=":        LTE,
+		">":         GT,
+		">=":        GTE,
+		"IN":        IN,
+		"NOT IN":    NOT_IN,
+		"MATCH":     MATCH,
+		"NOT MATCH": NOT_MATCH,
+	}
+	iteration := 0
+
+	conjoin := And
+	field, op, value := "", "", ""
+	cleanParse := VALID
+	returnF := func(message string) (Matcher, error) {
+		return nil, MatchParseError{Code: cleanParse, Message: message}
+	}
+	for iteration < len(tokens) {
+		switch {
+		case tokens[iteration] == "AND":
+			conjoin = And
+			cleanParse = UNFINISHED_MESSAGE
+		case tokens[iteration] == "OR":
+			conjoin = Or
+			cleanParse = UNFINISHED_MESSAGE
+		case field == "":
+			field = tokens[iteration]
+			if _, present := service.Fields[field]; !present && field != "_" {
+				cleanParse = UNKNOWN_FIELD
+				return returnF("Unknown Field provided: " + field)
+			}
+			cleanParse = UNFINISHED_MESSAGE
+		case op == "":
+			op = tokens[iteration]
+			cleanParse = UNFINISHED_MESSAGE
+		case op == "NOT":
+			op += " " + tokens[iteration]
+			cleanParse = UNFINISHED_MESSAGE
+		case op == "IN" || op == "NOT IN":
+			localIteration := iteration + 1
+			vals := make([]string, 0)
+			for localIteration < len(tokens) && tokens[localIteration] != ")" {
+				vals = append(vals, tokens[localIteration])
+				localIteration++
+			}
+			if localIteration >= len(tokens) {
+				return returnF("Could not parse contents of IN clause")
+			}
+			iteration = localIteration
+			step := And()
+			realOp, _ := Lookup[op]
+			if field == "_" {
+				step = And(step, FieldMatcher{Op: realOp, Value: value})
+			} else {
+				temp := StructMatcher{}
+				temp.AddField(field, FieldMatcher{Op: realOp, Value: value})
+				step = And(step, temp)
+			}
+			output = conjoin(output, step)
+			field, op, value = "", "", ""
+			cleanParse = VALID
+		default:
+			value = tokens[iteration]
+			step := And()
+			realOp, present := Lookup[op]
+			if !present {
+				cleanParse = INVALID_OPERATION
+				return returnF("Operation type is not supported: " + op)
+			}
+			if field == "_" {
+				step = And(step, FieldMatcher{Op: realOp, Value: value})
+			} else {
+				temp := StructMatcher{}
+				temp.AddField(field, FieldMatcher{Op: realOp, Value: value})
+				step = And(step, temp)
+			}
+			output = conjoin(output, step)
+			field, op, value = "", "", ""
+			cleanParse = VALID
+		}
+		iteration++
+	}
+	switch cleanParse {
+	case VALID:
+		return output, nil
+	case UNFINISHED_MESSAGE:
+		return returnF("The message has a trailing entry: " + tokens[iteration-1])
+	}
+	return output, nil
+}
+
+func Tokenize(message string) ([]string, error) {
+	output := make([]string, 0)
+	whitespace, _ := regexp.Compile("^[\\s,]+")
+	lParen, _ := regexp.Compile("^\\(")
+	rParen, _ := regexp.Compile("^\\)")
+	symbol, _ := regexp.Compile("^[a-zA-Z_]\\w*")
+	number, _ := regexp.Compile("^-?[0-9]+(\\.[0-9]+)?")
+	operators, _ := regexp.Compile("^[!=<>]+")
+	quote, _ := regexp.Compile("^\"(?:\\\\?.)*?\"")
+	for len(message) > 0 {
+		switch {
+		case whitespace.MatchString(message):
+			message = whitespace.ReplaceAllString(message, "")
+		case lParen.MatchString(message):
+			output = append(output, lParen.FindString(message))
+			message = lParen.ReplaceAllString(message, "")
+		case rParen.MatchString(message):
+			output = append(output, rParen.FindString(message))
+			message = rParen.ReplaceAllString(message, "")
+		case symbol.MatchString(message):
+			output = append(output, symbol.FindString(message))
+			message = symbol.ReplaceAllString(message, "")
+		case number.MatchString(message):
+			output = append(output, number.FindString(message))
+			message = number.ReplaceAllString(message, "")
+		case quote.MatchString(message):
+			output = append(output, quote.FindString(message))
+			message = quote.ReplaceAllString(message, "")
+		case operators.MatchString(message):
+			output = append(output, operators.FindString(message))
+			message = operators.ReplaceAllString(message, "")
+		default:
+			return output, MatchParseError{Code: TOKENIZE_ERROR, Message: message}
+		}
+	}
+	return output, nil
+}
---
+++ matcher/parse_test.go
@@ -0,0 +1,103 @@
+package goflect
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParseCodes(t *testing.T) {
+	render := func(input string, code ParseErrors) {
+		p := ParseStruct{Fields: map[string]int{"A": 1, "B": 2, "C": 3}}
+		_, e := p.Parse(input)
+		if e != nil {
+			err, _ := e.(MatchParseError)
+			if err.Code != code {
+				t.Errorf("Expected Error code %v, got %v", code, err.Code)
+			}
+		}
+	}
+
+	//Basic Operator Checks
+	render("", VALID)
+	render("_ = 1", VALID)
+	render("_ != 1", VALID)
+	render("_ < 1", VALID)
+	render("_ <= 1", VALID)
+	render("_ > 1", VALID)
+	render("_ >= 1", VALID)
+
+	render("_ IN (1, 2, 3)", VALID)
+	render("_ NOT IN (1, 2, 3)", VALID)
+
+	render("A = 1", VALID)
+	render("A = 1 AND B != 2", VALID)
+
+	//The unfinished Messages
+	render("A = 1 AND", UNFINISHED_MESSAGE)
+	render("A =", UNFINISHED_MESSAGE)
+	render("A", UNFINISHED_MESSAGE)
+	render("_ IN (1, 2, 3", UNFINISHED_MESSAGE)
+	render("_ NOT IN (1, 2, 3", UNFINISHED_MESSAGE)
+
+	//Invalid Operations
+	render("_ BACON 1", INVALID_OPERATION)
+
+	//Unknown Fields
+	render("D = 1", UNKNOWN_FIELD)
+	render("A = D", UNKNOWN_FIELD)
+
+}
+
+func TestTokenize(t *testing.T) {
+	expectedLen := func(s string, length int, code ParseErrors) {
+		tokens, e := Tokenize(s)
+		if e != nil {
+			err, _ := e.(MatchParseError)
+			if err.Code != code {
+				t.Errorf("Expected Error code %v, got %v", code, err.Code)
+			}
+			return
+		}
+		tokenCount := len(tokens)
+		if tokenCount != length {
+			fmt.Println(s)
+			fmt.Println(tokens)
+			t.Errorf("There are not enough tokens in expr: %v, expected %v, got %v", s, length, tokenCount)
+		}
+	}
+	expectedLen("", 0, VALID)
+	expectedLen("Bacon", 1, VALID)
+
+	//Check whitepsace
+	expectedLen("\tBacon\t", 1, VALID)
+	expectedLen("\tBacon\tPizza", 2, VALID)
+
+	//Commas are whitespace, facilitate tooling
+	expectedLen("\tBacon,Pizza", 2, VALID)
+
+	//Check paren tokenizing
+	expectedLen("Bacon (Pizza)", 4, VALID)
+	expectedLen("Bacon ( Pizza )", 4, VALID)
+
+	//Check operator tokenizing
+	expectedLen("Bacon = Pizza", 3, VALID)
+	expectedLen("_ = Pizza", 3, VALID)
+	expectedLen("_ = 1", 3, VALID)
+	expectedLen("Bacon=Pizza", 3, VALID)
+	expectedLen("Bacon!=Pizza", 3, VALID)
+
+	//Check number tokenizing
+	expectedLen("Bacon != 1", 3, VALID)
+	expectedLen("Bacon!=1", 3, VALID)
+	expectedLen("Bacon!=-1", 3, VALID)
+	expectedLen("Bacon!=1.0", 3, VALID)
+	expectedLen("Bacon!=-1.0", 3, VALID)
+
+	//Check quoting
+	expectedLen("Bacon!=\"Hi\"", 3, VALID)
+	expectedLen("Bacon!=\"Hi\\\"\"", 3, VALID)
+	//This has no trailing "
+	expectedLen("Bacon!=\"Hi", 3, TOKENIZE_ERROR)
+	//This has a trailing \"
+	expectedLen("Bacon!=\"Hi\\\"", 3, TOKENIZE_ERROR)
+}
