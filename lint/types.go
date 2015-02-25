32cfbc26bb868a782c3c1df95e023f3a369ad18d
Sean Devlin <francoisdevlin@gmail.com>
2015-02-25 11:46:26 -0500

    Added more usage documentation

--- lint/types.go
+++ lint/types.go
@@ -1,9 +1,21 @@
 /*
 There is a lot of imformation associated contained in the struct tags that the go compiler can't directly help us with.  This is a collection of tools to parse the struct tags and determine that they are valid.  There is also a tool to pretty print the struct tag information, in order to make is easier to read.
 
-For infomration on the linter, please read the ValidateType reference
+Using goflect-lint
 
-For information on the formatter, please read the FormatStructTag reference
+goflect-lint it the tool to use to verify that a program has properly formed annotations.  You can use it like so:
+
+    goflect-lint hello.go
+
+This will output any errors found in hello.go, and exit non-zero.  It will silently exist zero on success.  For infomration on the linter, please read the ValidateType reference
+
+Using goflect-format
+
+goflect-format is the tool to use in order to pretty print the struct tags.  You can use it like so:
+
+    goflect-format hello.go
+
+This will rewrite any struct tags using the pretty formatter.  This isn't quite 100% compatible with go fmt yet, so you may want to run go fmt on the code afterwards.  This is a known item to fix.  For information on the formatter, please read the FormatStructTag reference
 */
 package lint
 
