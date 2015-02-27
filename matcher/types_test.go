8ad30ffa74bde7375bcdde5304bff5014a90440c
Sean Devlin <francoisdevlin@gmail.com>
2015-02-27 11:04:22 -0500

    Added password struct

--- matcher/types_test.go
+++ matcher/types_test.go
@@ -96,3 +96,64 @@ func ExampleStructMatcher_2() {
 	//Record bar matches
 	//The dictionary matches
 }
+
+/*
+The struct matcher can compare values within a record as well.  This is accomplished with the Field method.  It must be called from the matcher that will be doing the comparison, or things will not be bound properly when Match is called
+*/
+func ExampleStructMatcher_3() {
+	type Password struct {
+		Current string
+		New     string
+		Repeat  string
+	}
+
+	storedPassword := "old secret"
+	matcher := NewStructMatcher()
+	matcher.AddField("Current", Eq(storedPassword))
+	matcher.AddField("New", And(
+		Eq(matcher.Field("Repeat")),
+		Neq(matcher.Field("Current")),
+	))
+
+	passwordRequest := Password{
+		Current: "old secret",
+		New:     "new secret",
+		Repeat:  "new secret",
+	}
+	if match, _ := matcher.Match(passwordRequest); match {
+		fmt.Println("The password request is well formed")
+	}
+
+	passwordRequest = Password{
+		Current: "mistake",
+		New:     "new secret",
+		Repeat:  "new secret",
+	}
+	if match, _ := matcher.Match(passwordRequest); !match {
+		fmt.Println("The password request failed, the current is wrong")
+	}
+
+	passwordRequest = Password{
+		Current: "old secreet",
+		New:     "new secret",
+		Repeat:  "wrong password repeated",
+	}
+	if match, _ := matcher.Match(passwordRequest); !match {
+		fmt.Println("The password request failed, the wrong password repeated")
+	}
+
+	passwordRequest = Password{
+		Current: "old secreet",
+		New:     "old secret",
+		Repeat:  "old secret",
+	}
+	if match, _ := matcher.Match(passwordRequest); !match {
+		fmt.Println("The password request failed, the secret was not changed")
+	}
+
+	//Output:
+	//The password request is well formed
+	//The password request failed, the current is wrong
+	//The password request failed, the wrong password repeated
+	//The password request failed, the secret was not changed
+}
