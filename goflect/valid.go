b187a1c9e0cdc9903d5785252117aa84e0dcf011
Sean Devlin <francoisdevlin@gmail.com>
2015-02-27 21:06:58 -0500

    Moved valid into goflect package

--- goflect/valid.go
+++ goflect/valid.go
@@ -1,24 +1,20 @@
-package validate
+package goflect
 
 import (
-	"fmt"
-	"git.sevone.com/sdevlin/goflect.git/goflect"
 	"git.sevone.com/sdevlin/goflect.git/matcher"
 	"reflect"
 )
 
 func DefaultMatcher(record interface{}) (matcher.Matcher, error) {
 	output := matcher.Any()
-	fields := goflect.GetInfo(record)
+	fields := GetInfo(record)
 	kinds := make(map[string]reflect.Kind)
 	for _, field := range fields {
 		kinds[field.Name] = field.Kind
 	}
 	p := matcher.ParseStruct{Fields: kinds}
 	for _, field := range fields {
-		fmt.Println(field)
-		match, err := p.Parse("")
-		//match, err := p.Parse(field.ValidExpr)
+		match, err := p.Parse(field.ValidExpr)
 		if err != nil {
 			return nil, err
 		}
---
+++ goflect/valid_test.go
@@ -0,0 +1,66 @@
+package goflect
+
+import (
+	"fmt"
+	"git.sevone.com/sdevlin/goflect.git/matcher"
+)
+
+func ExampleDefaultMatcher_1() {
+	type Password struct {
+		Current string
+		New     string `valid:"New != Current AND New = Repeat"`
+		Repeat  string
+	}
+
+	def, err := DefaultMatcher(&Password{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	storedPassword := "old secret"
+	old := matcher.NewStructMatcher()
+	old.AddField("Current", matcher.Eq(storedPassword))
+	m := matcher.And(def, old)
+
+	passwordRequest := Password{
+		Current: "old secret",
+		New:     "new secret",
+		Repeat:  "new secret",
+	}
+	if match, _ := m.Match(passwordRequest); match {
+		fmt.Println("The password request is well formed")
+	}
+
+	passwordRequest = Password{
+		Current: "mistake",
+		New:     "new secret",
+		Repeat:  "new secret",
+	}
+	if match, _ := m.Match(passwordRequest); !match {
+		fmt.Println("The password request failed, the current is wrong")
+	}
+
+	passwordRequest = Password{
+		Current: "old secreet",
+		New:     "new secret",
+		Repeat:  "wrong password repeated",
+	}
+	if match, _ := m.Match(passwordRequest); !match {
+		fmt.Println("The password request failed, the wrong password repeated")
+	}
+
+	passwordRequest = Password{
+		Current: "old secreet",
+		New:     "old secret",
+		Repeat:  "old secret",
+	}
+	if match, _ := m.Match(passwordRequest); !match {
+		fmt.Println("The password request failed, the secret was not changed")
+	}
+
+	//Output:
+	//The password request is well formed
+	//The password request failed, the current is wrong
+	//The password request failed, the wrong password repeated
+	//The password request failed, the secret was not changed
+}
