bd4a3ae6f808bf40548a6fa636b4193ee07dbbda
Sean Devlin <francoisdevlin@gmail.com>
2015-03-03 07:38:01 -0500

    Added example 2, basic query test

--- sql/types_test.go
+++ sql/types_test.go
@@ -3,6 +3,7 @@ package records
 import (
 	"database/sql"
 	"fmt"
+	"git.sevone.com/sdevlin/goflect.git/matcher"
 	"reflect"
 )
 
@@ -90,4 +91,82 @@ func Example_reference() {
 	//Output:
 	//Devices
 	//{1 Device 1}
+}
+
+/*
+Let's see how easy it is to insert records into a blank database
+*/
+func Example_1() {
+	//Create a database with blank tables, for example only
+	service := tableCreationBoilerplate()
+
+	//Insert a device into the database
+	service.Create(Device{Name: "Device 1"})
+
+	device := Device{}
+	//Create an iterator over all of the devices
+	next, _ := service.ReadAll(device)
+	fmt.Println("Devices")
+	//Iterate over the deivces
+	for next(&device) {
+		fmt.Println(device)
+	}
+
+	//Output:
+	//Devices
+	//{1 Device 1}
+}
+
+/*
+Let's insert a few devices into the database, and the perform a query on the database
+*/
+func Example_2() {
+	//Create a database with blank tables, for example only
+	service := tableCreationBoilerplate()
+
+	//Insert a device into the database
+	service.Create(Device{Name: "Device 1"})
+	service.Create(Device{Name: "Device 2"})
+	service.Create(Device{Name: "Device 3"})
+
+	device := Device{}
+	//Create an iterator over all of the devices
+	next, _ := service.ReadAll(device)
+	fmt.Println("Devices")
+	//Iterate over the deivces
+	for next(&device) {
+		fmt.Println(device)
+	}
+
+	//Let's find device 1
+	//We'll need to create a matcher the describes the set that this device is in
+	query := matcher.NewStructMatcher()
+	query.AddField("Name", matcher.Eq("Device 1"))
+	next, _ = service.ReadAllWhere(device, query)
+
+	fmt.Println("Our device")
+	//Iterate over the deivces
+	for next(&device) {
+		fmt.Println(device)
+	}
+
+	//Finding a device that doesn't exist will have the iterator quit immediately
+	query = matcher.NewStructMatcher()
+	query.AddField("Name", matcher.Eq("DOES NOT EXIST"))
+	next, _ = service.ReadAllWhere(device, query)
+
+	fmt.Println("No Devices Match Query")
+	//Iterate over the deivces
+	for next(&device) {
+		fmt.Println(device)
+	}
+
+	//Output:
+	//Devices
+	//{1 Device 1}
+	//{2 Device 2}
+	//{3 Device 3}
+	//Our device
+	//{1 Device 1}
+	//No Devices Match Query
 }
