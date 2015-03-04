4cc6f649faeb80dbfa450b2a0e53fd481ce599c8
Sean Devlin <francoisdevlin@gmail.com>
2015-03-04 15:42:56 -0500

    Removed some fat from the struct matcher

--- matcher/struct_matcher.go
+++ matcher/struct_matcher.go
@@ -11,27 +11,33 @@ type fieldYielder struct {
 	Name    string
 }
 
-func (y fieldYielder) Yield() (interface{}, error) {
-	record := y.matcher.record
+func lookup(record interface{}, name string) (interface{}, error) {
 	switch r := record.(type) {
 	case map[string]interface{}:
-		output, ok := r[y.Name]
+		output, ok := r[name]
 		if !ok {
 			return nil, InvalidCompare(2)
 		}
 		return output, nil
 	default:
 		val := reflect.ValueOf(r)
+		if val.Kind() == reflect.Ptr {
+			val = val.Elem()
+		}
 		if !val.IsValid() {
 			return nil, InvalidCompare(2)
 		}
-		rVal := val.FieldByName(y.Name)
+		rVal := val.FieldByName(name)
 		if !rVal.IsValid() {
 			return nil, InvalidCompare(2)
 		}
 		return rVal.Interface(), nil
 	}
-	return nil, InvalidCompare(2)
+}
+
+func (y fieldYielder) Yield() (interface{}, error) {
+	record := y.matcher.record
+	return lookup(record, y.Name)
 }
 
 /*
@@ -58,47 +64,20 @@ func (field *structMatcher) Match(record interface{}) (bool, error) {
 		field.Fields = make(map[string]Matcher)
 	}
 	field.record = record
-	switch r := record.(type) {
-	case map[string]interface{}:
-		valid := true
-		for name, matcher := range field.Fields {
-			attr, present := r[name]
-			if !present {
-				return false, InvalidCompare(1)
-			}
-			localMatch, err := matcher.Match(attr)
-			if err != nil {
-				return localMatch, err
-			}
-			valid = valid && localMatch
-			if !valid {
-				return false, nil
-			}
+	valid := true
+	for name, matcher := range field.Fields {
+		attr, err := lookup(record, name)
+		if err != nil {
+			return false, InvalidCompare(1)
 		}
-		return true, nil
-	default:
-		val := reflect.ValueOf(record)
-		if val.Kind() == reflect.Ptr {
-			val = val.Elem()
+		localMatch, err := matcher.Match(attr)
+		if err != nil {
+			return localMatch, err
 		}
-		valid := true
-
-		for name, matcher := range field.Fields {
-			attr := val.FieldByName(name)
-			if !attr.IsValid() {
-				return false, InvalidCompare(1)
-			}
-			localMatch, err := matcher.Match(attr.Interface())
-			if err != nil {
-				return localMatch, err
-			}
-			valid = valid && localMatch
-			if !valid {
-				return false, nil
-			}
+		valid = valid && localMatch
+		if !valid {
+			return false, nil
 		}
-		return true, nil
 	}
-
-	return false, InvalidCompare(1)
+	return true, nil
 }
